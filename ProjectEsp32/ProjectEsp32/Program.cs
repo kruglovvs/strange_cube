4d7114844712cc9f52770b3a4198d2bddd6b2f60
kruglovvs <kruglov.valentine@gmail.com>
2023-07-20 20:04:22 +0300

    I need new remaster: delete all this shit and make it simple
    remade main

--- ProjectEsp32/ProjectEsp32/Program.cs
+++ ProjectEsp32/ProjectEsp32/Program.cs
@@ -3,33 +3,39 @@ using Periphery;
 using System.Diagnostics;
 using System.Threading;
 using System.Device.Gpio;
+using Iot.Device.Button;
 
 namespace ProjectESP32
 {
     public class Program
     {
+        internal static GpioController GpioController = new GpioController();
         public static void Main()
         {
-            //Debug.WriteLine($"net connected: {NetworkController.Connect()}");
-            PeripheryController.TurnOn();
-            PeripheryController.GasSensored += () => { Debug.WriteLine("Vib sensored"); };
-            Debug.WriteLine($"is vn sensor's pin can pe input: {PeripheryController.GpioController.IsPinModeSupported(39, PinMode.Input)}");
-            Debug.WriteLine($"is vn sensor's pin opened: {PeripheryController.GpioController.IsPinOpen(39)}");
+            //  Open all pins.
 
-            //NetworkController.Subscribe("/GameData");
-            //Debug.WriteLine($"is connected{ NetworkController.IsConnected}");
-            /*NetworkController.Got += (sender, e) => { Debug.WriteLine($" topic: {e.Topic} message: {e.Message}"); };
-            for (int i = 0; i < 10; i++)
-            {
-                NetworkController.Publish("/GameData", "kek");
-            }*/
+            GpioController.OpenPin(12, PinMode.Output); // A0.
+            GpioController.OpenPin(13, PinMode.Output); // MOSI.
+            GpioController.OpenPin(14, PinMode.Output); // CLK.
+            GpioController.OpenPin(15, PinMode.Output); // BOut1.
+            GpioController.OpenPin(16, PinMode.Input);  // BIn3.
+            GpioController.OpenPin(17, PinMode.Input);  // BIn2.
+            GpioController.OpenPin(19, PinMode.Input);  // Int1.
+            GpioController.OpenPin(21, PinMode.Output); // SDA.
+            GpioController.OpenPin(22, PinMode.Output); // SCL.
+            GpioController.OpenPin(23, PinMode.Input);  // Int2.
+            GpioController.OpenPin(26, PinMode.Output); // ARes.
+            GpioController.OpenPin(27, PinMode.Output); // ACs.
 
+            GpioController.OpenPin(34, PinMode.Output); // Door.
+            GpioController.OpenPin(35, PinMode.Output); // VibrationMotor.
 
-            Thread.Sleep(Timeout.Infinite);
+            GpioController.OpenPin(25, PinMode.Input).ValueChanged += (sender, e) => { Debug.WriteLine("1"); };  // PhotoSensor.
+            GpioController.OpenPin(36, PinMode.Input).ValueChanged += (sender, e) => { Debug.WriteLine("1"); };  // GasSensor.
+            GpioController.OpenPin(39, PinMode.Input).ValueChanged += (sender, e) => { Debug.WriteLine("1"); };  // VibrationSensor.
 
-            // Browse our samples repository: https://github.com/nanoframework/samples
-            // Check our documentation online: https://docs.nanoframework.net/
-            // Join our lively Discord community: https://discord.gg/gCyBu8T
+            new GpioButton(33, GpioController, false).Press += (sender, e) => { Debug.WriteLine("1"); }; // B1
+            new GpioButton(32, GpioController, false).Press += (sender, e) => { Debug.WriteLine("1"); }; // B2
         }
     }
 }
